bc57cd593723254ec92fa5689d3dc1cf05e98659
Jackson Benete <jacksonbenete@gmail.com>
2022-01-29 13:42:22 -0300

    add new functions for controlling the clock

--- vpk/main.c
+++ vpk/main.c
@@ -7,6 +7,8 @@
 #define printf(...) psvDebugScreenPrintf(__VA_ARGS__)
 
 static void wait_key_press();
+static void set_default_clock();
+static void set_lower_clock();
 
 int main(int argc, char *argv[])
 {
@@ -27,17 +29,11 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	scePowerSetArmClockFrequency(111);
-	scePowerSetBusClockFrequency(111);
-	scePowerSetGpuClockFrequency(111);
-	scePowerSetGpuXbarClockFrequency(111);
+    set_lower_clock();
 
 	wait_key_press("START + SELECT", SCE_CTRL_START | SCE_CTRL_SELECT);
 
-	scePowerSetArmClockFrequency(266);
-	scePowerSetBusClockFrequency(166);
-	scePowerSetGpuClockFrequency(166);
-	scePowerSetGpuXbarClockFrequency(111);
+    set_default_clock();
 
 	vitastick_stop();
 
@@ -47,13 +43,58 @@ int main(int argc, char *argv[])
 void wait_key_press(const char *key_desc, unsigned int key_mask)
 {
 	SceCtrlData pad;
+    int ret;
+    unsigned int lower_clock_mask, default_clock_mask, reload_vitastick_mask;
+
+    lower_clock_mask = SCE_CTRL_SELECT | SCE_CTRL_VOLDOWN;
+    default_clock_mask = SCE_CTRL_SELECT | SCE_CTRL_VOLUP;
+    reload_vitastick_mask = SCE_CTRL_SELECT | SCE_CTRL_TRIANGLE;
 
 	printf("Press %s to exit.\n", key_desc);
 
 	while (1) {
 		sceCtrlReadBufferPositive(0, &pad, 1);
+
+        // lower clock
+        if ((pad.buttons & lower_clock_mask) == lower_clock_mask)
+        {
+            printf("reducing clock...\n");
+            set_lower_clock();
+            printf("clock reduced [111,111,111,111]\n");
+        }
+        // default clock
+        if ((pad.buttons & default_clock_mask) == default_clock_mask)
+        {
+            printf("recovering default clock...\n")
+            set_default_clock();
+            printf("clock recovered [266,166,166,111]\n")
+        }
+        // reload app
+        if ((pad.buttons & reload_vitastick_mask) == reload_vitastick_mask)
+        {
+            vitastick_stop();
+            ret = vitastick_start();
+            if (ret >= 0) printf("vitastick started successfully!\n");
+        }
+
 		if ((pad.buttons & key_mask) == key_mask)
 			break;
 		sceKernelDelayThreadCB(200 * 1000);
 	}
+}
+
+void set_default_clock()
+{
+    scePowerSetArmClockFrequency(266);
+    scePowerSetBusClockFrequency(166);
+    scePowerSetGpuClockFrequency(166);
+    scePowerSetGpuXbarClockFrequency(111);
+}
+
+void set_lower_clock()
+{
+    scePowerSetArmClockFrequency(111);
+    scePowerSetBusClockFrequency(111);
+    scePowerSetGpuClockFrequency(111);
+    scePowerSetGpuXbarClockFrequency(111);
 }
